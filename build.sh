ad519b741d22c34d720dbc1f32fb6d3fbc130bec
liuwei <liuwei3-s@360.cn>
2017-12-04 14:07:21 +0800

    update the build.sh script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc kafka_offset_tools.cc kafka_offset_resetter.cc -I/home/test_user_2/works/qbus_2-0/client/cxx/thirdparts/librdkafka/src -Wl,-dn  -lboost_program_options -L/usr/local/lib -lrdkafka -Wl,-dy  -lpthread -lm -lstdc++ -lrt -lz  -o kafka_offset_tools
+gcc kafka_offset_tools.cc kafka_offset_resetter.cc  -Wl,-dn  -lboost_program_options -L/usr/local/lib -lrdkafka -Wl,-dy  -lpthread -lm -lstdc++ -lrt -lz  -o kafka_offset_tools
